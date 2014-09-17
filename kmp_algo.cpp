ba7733a1209ee2331775d3716f8200b1d75b19ea
yarr <yarr@debian-yaroslav.localdomain>
2014-09-17 17:19:52 +0400

    libc version

--- kmp_algo.cpp
+++ kmp_algo.cpp
@@ -3,12 +3,25 @@
 #include <string>
 #include <cassert>
 
+#include <gnu/libc-version.h>
+
+
+namespace NmUtil {
+
+void printLibcInfo (std::ostream & cout) {
+	cout << "Version: " << gnu_get_libc_version () << '\n';
+	cout << "Release: " << gnu_get_libc_release () << '\n';
+	
+	return;
+}
+
+}
 
 
 int main (int argc, char *argv []) {
+	;
 	
-	
-	
+	NmUtil::printLibcInfo (std::cout);
 	
 	return 0;
 }
