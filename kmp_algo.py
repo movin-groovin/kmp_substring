792d6b68de3cfeca79d73ddd0b52595f061a7ebb
yarr <yarr@debian>
2014-12-16 14:43:08 +0000

    finish

--- kmp_algo.py
+++ kmp_algo.py
@@ -2,7 +2,9 @@
 #-*-  coding:utf-8 -*-
 
 
-import sys
+import sys,\
+	   os,\
+	   pdb
 
 
 class KmpAlgo (object):
@@ -67,13 +69,7 @@ class KmpAlgo (object):
 		return found
 
 
-
-def main():
-	text = 'If an argument xweak is passed, and x is not empty, the weak_ptr object becomes weak part of the owning group weak'
-	pattern = 'weak'
-	kmpObj = KmpAlgo (pattern)
-	res = kmpObj.Search (text)
-	
+def PrintResults(pattern, res):
 	if res:
 		print ("Pattern: %s" % (pattern))
 		for i, val in enumerate (res):
@@ -81,8 +77,46 @@ def main():
 	else:
 		print ("Nothing have found")
 		
+	return 0
+
+
+def Test():
+	text = 'If an argument xweak is passed, and x is not empty, the weak_ptr object becomes weak part of the owning group weak'
+	pattern = 'weak'
+	kmpObj = KmpAlgo (pattern)
+	
+	PrintResults(pattern, kmpObj.Search (text))
+		
 	#print kmpObj._KmpAlgo__NaiveMakePiArr('ababababca')
 	#print kmpObj._KmpAlgo__MakePiArr('ababababca')
+		
+	return 0
+
+
+def FindAtFile(ptrn, flName):
+	kmp = KmpAlgo(ptrn)
+	#print kmp._KmpAlgo__MakePiArr(ptrn)
+	#return 0
+	
+	with file (flName, 'r') as f:
+		buf = f.read()
+		print ("File content length: {}\n\n".format (len(buf)))
+		PrintResults(ptrn, kmp.Search(buf))
+	
+	return 0
+
+
+def main():
+	pattern = ''; fileName = ''
+	
+	#Test()
+	if len(sys.argv) < 3:
+		print ("Enter: ./kmp_algo.py <pattern> <file_for_search>")
+		return 10001
+	else:
+		pattern = sys.argv[1]
+		fileName = sys.argv[2]
+	FindAtFile(pattern, fileName)
 		
 	return 0
 
